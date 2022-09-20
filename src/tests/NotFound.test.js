410e85057b0a618c03482fc64b51a45904819df7
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-09-20 14:12:34 -0300

    requisito 4 feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste, página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+
+    const h2NotFound = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    }, { level: 2 });
+
+    expect(h2NotFound).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+
+    const srcText = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const img = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+
+    expect(img).toHaveAttribute('src', srcText);
+  });
+});
